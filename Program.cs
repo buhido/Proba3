731192d10020aa6f82b4438af2bf0b5cff9cce25
vladimir golovin <tkomplect@mail.ru>
2022-12-10 19:21:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.NetworkInformation;
+
+Console.WriteLine("Hello, World!");
+
+
+// Задача 19 Базовые алгоритмы
+//Console.Write("Введите пятизначное значение: ");
+//int i = int.Parse(Console.ReadLine());
+//string array = i.ToString(); // Строковое представление , например: 23432
+//if (array[0] == array[4] || array[1] == array[3])
+//{
+//    Console.WriteLine("Это палиндром: " + i);
+//}
+//else
+//{
+//    Console.WriteLine(i + " => Не является палиндромом");
+//}
+
+
+
+//Задача 21 Найти расстояние между двумя точками с заданными координатами в 3Д пространстве.
+// формула квадрата расст. между двумя точками в 3Д  s^2=(z2-z1)^2+(y2-y1)^2+(x2-x1)^2
+
+//Console.WriteLine("Введите координаты точки А :");
+//Console.WriteLine("Координата x1");
+//double x1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Координата y1");
+//double y1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Координата Z1");
+//double z1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите координаты точки В :");
+//Console.WriteLine("Координата x2");
+//double x2 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Координата y2");
+//double y2 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Координата Z2");
+//double z2 = double.Parse(Console.ReadLine());
+//double s = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+//Console.WriteLine($"Расстояние между точками {s}");
+
+
+
+// Задача 23  Выдать таблицу кубов чисел от 1 до N
+
+
+//Console.WriteLine("Введите число");
+//int N = int.Parse(Console.ReadLine());
+//for (int i = 1; i <= N; i++)
+//{
+//    Console.Write("Cube: ");
+//    Console.WriteLine($"{Math.Pow(i, 3)}, ");
+//}
+
+
